5c125546052efb96fb059a35896d600d47629702
Spector323 <154788903+Spector323@users.noreply.github.com>
2024-06-28 14:47:06 +0300

    menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,69 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const products = [
-        {
-            id: 0,
-            imgSrc: "./assets/images/image/1.png",
-            productName: "Ягненок",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "620 ₽"
-        },
-        {
-            id: 1,
-            imgSrc: "./assets/images/image/2.png",
-            productName: "Индейка",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "450 ₽"
-        },
-        {
-            id: 2,
-            imgSrc: "./assets/images/image/3.png",
-            productName: "Гусь",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "7900 ₽"
-        },
-        {
-            id: 3,
-            imgSrc: "./assets/images/image/4.png",
-            productName: "Утка",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "3230 ₽"
-        },
-        {
-            id: 4,
-            imgSrc: "./assets/images/image/1.png",
-            productName: "Ягненок",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "620 ₽"
-        },
-        {
-            id: 5,
-            imgSrc: "./assets/images/image/2.png",
-            productName: "Индейка",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "450 ₽"
-        },
-        {
-            id: 6,
-            imgSrc: "./assets/images/image/3.png",
-            productName: "Гусь",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "7900 ₽"
-        },
-        {
-            id: 7,
-            imgSrc: "./assets/images/image/4.png",
-            productName: "Утка",
-            productWeight: "Вес: 225 г",
-            productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком",
-            productPrice: "3230 ₽"
-        }
+        { id: 0, imgSrc: "./assets/images/image/1.png", productName: "Ягненок", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "620 ₽" },
+        { id: 1, imgSrc: "./assets/images/image/2.png", productName: "Индейка", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "450 ₽" },
+        { id: 2, imgSrc: "./assets/images/image/3.png", productName: "Гусь", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "7900 ₽" },
+        { id: 3, imgSrc: "./assets/images/image/4.png", productName: "Утка", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "3230 ₽" },
+        { id: 4, imgSrc: "./assets/images/image/1.png", productName: "Ягненок", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "620 ₽" },
+        { id: 5, imgSrc: "./assets/images/image/2.png", productName: "Индейка", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "450 ₽" },
+        { id: 6, imgSrc: "./assets/images/image/3.png", productName: "Гусь", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "7900 ₽" },
+        { id: 7, imgSrc: "./assets/images/image/4.png", productName: "Утка", productWeight: "Вес: 225 г", productDescription: "Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком", productPrice: "3230 ₽" }
     ];
 
     function createProductCards(array) {
@@ -104,5 +48,23 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         mousewheel: true,
         keyboard: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
     });
 });
